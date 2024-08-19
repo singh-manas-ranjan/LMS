026ef577321bc18e95379309e41690bed816f5a4
Manas Ranjan Singh <manassingh900@gmail.com>
2024-08-19 21:08:07 +0530

    changed to fetch api

--- src/actions/adminAccess/adminAccessAction.ts
+++ src/actions/adminAccess/adminAccessAction.ts
@@ -10,26 +10,50 @@ export async function fetchUserById(
   role: "students" | "instructors"
 ): Promise<TUser> {
   try {
-    const cookieStore = cookies();
-    const accessToken = cookieStore.get("accessToken")?.value;
+    // const cookieStore = cookies();
+    // const accessToken = cookieStore.get("accessToken")?.value;
 
-    if (!accessToken) {
-      console.error("No access token found in cookies");
-      return {} as TUser;
-    }
+    // if (!accessToken) {
+    //   console.error("No access token found in cookies");
+    //   return {} as TUser;
+    // }
 
-    const response = await axios.get(
-      // `http://localhost:3131/api/v1/admin/access/${role}/${studentId}`,
-      `https://learnopia-backend.vercel.app/api/v1/admin/access/${role}/${studentId}`,
+    // const response = await axios.get(
+    //   `http://localhost:3131/api/v1/admin/access/${role}/${studentId}`,
+    //   // `https://learnopia-backend.vercel.app/api/v1/admin/access/${role}/${studentId}`,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${accessToken}`,
+    //     },
+    //     withCredentials: true,
+    //   }
+    // );
+
+    // return response.data.body as TUser;
+    const response = await fetch(
+      `http://localhost:3131/api/v1/admin/access/${role}/${studentId}`,
+      // `https://learnopia-backend.vercel.app/api/v1/admin/access/${role}/${studentId}`,
       {
+        method: "GET",
         headers: {
-          Authorization: `Bearer ${accessToken}`,
+          Cookie: cookies().toString(),
         },
-        withCredentials: true,
       }
     );
 
-    return response.data.body as TUser;
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    console.log(responseData);
+
+    if (!responseData.success) {
+      console.error("API response status is not true:", responseData.message);
+      return {} as TUser; // Returning an empty user object if status is not true
+    }
+
+    return responseData.body as TUser; // Return only the user object
   } catch (error) {
     console.error("Error fetching student by ID:", error);
     // redirect(`/admin-dashboard/accounts/${role}`);
